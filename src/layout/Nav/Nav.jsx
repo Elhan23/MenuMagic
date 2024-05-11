bdf6c9ef1e7dbe96a4cb803872ec390393d2ad51
Elhan23 <elhan13chik@gmail.com>
2024-05-11 00:40:36 -0800

    update compnent

--- src/layout/Nav/Nav.jsx
+++ src/layout/Nav/Nav.jsx
@@ -1,39 +1,16 @@
-import React from "react";
-import "./Nav.scss";
-import { NavLink } from "react-router-dom";
-import logo from "../../assets/images/Logo.svg";
-import { CiShoppingBasket } from "react-icons/ci";
+import React from 'react';
+import logo from '../../assets/images/menumagiclogo.png'
 
 function Nav() {
   return (
-    <div className="container">
-      <div className="navigation_flex">
-        <div className="logo_width">
-          <NavLink to="/">
-            <img src={logo} alt="logo" />
-          </NavLink>
+    <div>
+        <div className='container'>
+               <div>
+                  <img src={logo} alt="logo" />
+               </div>
         </div>
-
-        <ul>
-          <li>
-            <NavLink to="/about">About</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact</NavLink>
-          </li>
-        </ul>
-
-        <ul>
-          <li>
-            <CiShoppingBasket />
-            <NavLink to="/registration">
-              <button>Log In</button>
-            </NavLink>
-          </li>
-        </ul>
-      </div>
     </div>
-  );
+  )
 }
 
-export default Nav;
+export default Nav
