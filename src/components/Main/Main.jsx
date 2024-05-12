e0769d14814407d6c9abd4acb943cee7535f346b
Elhan23 <elhan13chik@gmail.com>
2024-05-11 17:33:30 -0800

    update main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,12 @@
-import TabComponent from "../TabComponent/TabComponent";
-import "./Main.scss";
+import "./Main.scss"
 
 function Main() {
   return (
     <main>
-      <div className="work_block">
-        <div className="find_class">
-          <h2>Find the best solution for yourself</h2>
-        </div>
-        <hr />
-        <TabComponent/>
+      <div className="background_section">
+           <section className="">
+                 wqefrgtyju
+           </section>
       </div>
     </main>
   );
