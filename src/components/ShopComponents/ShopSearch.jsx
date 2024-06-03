7d4ec476d2ecfb82f47bd38c9c35974b166a9e66
Elhan23 <elhan13chik@gmail.com>
2024-06-03 19:49:18 +0600

    add react icons new

--- src/components/ShopComponents/ShopSearch.jsx
+++ src/components/ShopComponents/ShopSearch.jsx
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 import logo from "../../assets/images/icons/logo.svg";
 import { FaSearch } from "react-icons/fa";
 import { FaRegCircleUser } from "react-icons/fa6";
+import { RiShoppingBasket2Fill } from "react-icons/ri";
 import "./ShopSearch.scss";
 
 function ShopSearch() {
@@ -13,16 +14,20 @@ function ShopSearch() {
           <p>Menu Magic</p>
         </div>
 
-        <div className="shopsearch-search-user">
+        <section className="shopsearch-search-user">
           <div className="shopsearch-search-bar">
             <input type="search" placeholder="Search..." />
             <FaSearch className="shopsearch-search-icon" />
           </div>
+
+          <span className="FaShoppingBasket_bg">
+          <RiShoppingBasket2Fill />
+          </span>
           <FaRegCircleUser className="shopsearch-user-icon" />
-        </div>
+        </section>
       </div>
     </nav>
   );
 }
 
-export default  ShopSearch;
+export default ShopSearch;
