bbc8e99c5c846a7fd2278e8ff106556c35465d86
Elhan23 <elhan13chik@gmail.com>
2024-06-04 18:50:03 +0600

    chabges projects

---
+++ src/components/PartnersComponents/PartnersComponents.jsx
@@ -0,0 +1,11 @@
+import './PartnersComponents/scss'
+
+function PartnersComponents() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default PartnersComponents
---
+++ src/pages/Appside/SavedRecipe/SavedRecipe.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SavedRecipe() {
+  return (
+    <div>
+       recipe
+    </div>
+  )
+}
+
+export default SavedRecipe
