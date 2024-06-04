313a61d404d80d18135a731ba53cefcf005da090
Elhan23 <elhan13chik@gmail.com>
2024-06-05 00:15:25 +0600

    cahnges store and actions

---
+++ src/store/actions/getUser/getUser.js
@@ -0,0 +1,55 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getUser = createAsyncThunk(
+  "user/getUser",
+  async (userData, { rejectWithValue }) => {
+    try {
+      const res = await axios.post(process.env.USER_POST_ZAPROS, userData);
+      const jwtToken = res.headers.authorization;
+      return { userData: res.data, jwtToken };
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    loading: false,
+    user: null,
+    jwtToken: null,
+    error: "",
+  },
+  reducers: {
+    setJWTToken(state, action) {
+      state.jwtToken = action.payload;
+      localStorage.setItem("jwtToken", action.payload);
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getUser.pending, (state) => {
+        state.loading = true;
+        state.error = "";
+      })
+      .addCase(getUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload.userData;
+        state.jwtToken = action.payload.jwtToken;
+        if (action.payload.jwtToken) {
+          state.jwtToken = action.payload.jwtToken;
+          localStorage.setItem("jwtToken", action.payload.jwtToken);
+        }
+      })
+      .addCase(getUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export const { setJWTToken } = userSlice.actions;
+
+export default userSlice.reducer;
---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { getUser } from "../store/actions/getUser/getUser";
+
+export const store = configureStore({
+  reducer: {
+    user: getUser,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(false),
+});
+
+export default store;
